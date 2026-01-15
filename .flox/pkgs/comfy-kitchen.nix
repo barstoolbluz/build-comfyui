96cab0cf185502825fd3307666161b5d7455859e
barstoolbluz <steve@flox.dev>
2026-01-15 15:42:15 -0500

    Fix comfy-kitchen package definition for source build

--- .flox/pkgs/comfy-kitchen.nix
+++ .flox/pkgs/comfy-kitchen.nix
@@ -3,22 +3,22 @@
 python3Packages.buildPythonPackage rec {
   pname = "comfy-kitchen";
   version = "0.2.6";
-  format = "wheel";
+  format = "setuptools";
 
   src = fetchPypi {
     pname = "comfy_kitchen";
-    version = version;
-    dist = "py3";
-    python = "py3";
-    format = "wheel";
-    hash = "sha256-1d9sd8daasm3gi50wh6l207q2xxdkfyhv1512h7h4d6s2b2yscrp";
+    inherit version;
+    hash = "sha256-sPx5+K6x7sE9MNKDBzSmD6CcKBsJmwtq+qDu9h0vNBs=";
   };
+
+  # No dependencies listed
+  propagatedBuildInputs = [ ];
 
   pythonImportsCheck = [ "comfy_kitchen" ];
 
   meta = with lib; {
     description = "ComfyUI kitchen utilities for FP8/FP4 support";
-    homepage = "https://github.com/comfyanonymous/ComfyUI";
+    homepage = "https://pypi.org/project/comfy-kitchen/";
     license = licenses.gpl3Only;
     maintainers = [ ];
   };
